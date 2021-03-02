bc2f3c06796e5a4b49a85fb3fe024dc5d445de69
Rahul Sharma <rahulsharma841990@outlook.com>
2021-03-02 11:53:03 +0530

    setup loading and API

--- src/app/teams/team-create/team-create.page.ts
+++ src/app/teams/team-create/team-create.page.ts
@@ -1,15 +1,24 @@
+import { NavController } from '@ionic/angular';
+import { UtilsService } from './../../utils.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-team-create',
-  templateUrl: './team-create.page.html',
-  styleUrls: ['./team-create.page.scss'],
+	selector: 'app-team-create',
+	templateUrl: './team-create.page.html',
+	styleUrls: ['./team-create.page.scss'],
 })
 export class TeamCreatePage implements OnInit {
 
-  constructor() { }
+	constructor(public utilService: UtilsService, public navCtrl: NavController) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
+	addTeam() {
+		this.utilService.presentLoadingWithOptions().then((a: any) => {
+			this.utilService.loading.onDidDismiss().then(() => {
+				this.navCtrl.navigateForward('type-of-team', { animationDirection: 'forward' });
+			});
+		});
+	}
 }

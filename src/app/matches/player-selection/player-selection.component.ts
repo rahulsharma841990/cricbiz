7c36b5e83fd9f870292662d4310b0a90775631a9
Rahul Sharma <rahulsharma841990@outlook.com>
2021-05-19 15:58:56 +0530

    fixed scoring issues

--- src/app/matches/player-selection/player-selection.component.ts
+++ src/app/matches/player-selection/player-selection.component.ts
@@ -10,8 +10,15 @@ export class PlayerSelectionComponent implements OnInit {
 
 	@Input() type;
 	@Input() number;
+	public title: any;
 	listOfPlayers: any = [];
-	constructor(private modalController: ModalController) { }
+	constructor(private modalController: ModalController) {
+		if (this.type == 'Bat') {
+			this.title = 'Select Batsman';
+		} else if (this.type == 'Bowler') {
+			this.title = 'Select Bowler';
+		}
+	}
 
 	ngOnInit() {
 		let listOfPlayers = JSON.parse(localStorage.getItem('match_id'));
